4e2c2aa659fdb58e6fc1702f6bc4fcd7acc6e13f
It Dterra <it@dterra.eu>
2024-02-29 16:29:34 +0300

    update

--- src/routes/images/index.tsx
+++ src/routes/images/index.tsx
@@ -1,16 +1,14 @@
 import React from "react"
-import img1 from '../../assets/img1.png'
-import img2 from '../../assets/img2.png'
 
 const PageImages = () => {
   return (
     <div className="container-fluid">
       <div className="row align-items-center justify-content-center">
         <div className="col-6">
-          <img src={img1} alt="" />
+          <img src='' alt="" />
         </div>
         <div className="col-6">
-          <img src={img2} alt="" />
+          <img src='' alt="" />
         </div>
       </div>
     </div>
