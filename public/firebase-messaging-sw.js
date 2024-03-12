96299bfbeaacf93bf65603ba66d0d8439f59b853
etatarintsev <service@dterra.ru>
2024-03-12 09:44:08 +0300

    [master] 3

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,7 +1,8 @@
 self.addEventListener('push', (event) => {
   const options = {
-    body: event.data.text(),
-    icon: '/logo192.png', // Replace with your icon path
+    // body: event.data.text(),
+    body: 'Вам написали в чате поддержки',
+    icon: '/icon175x175.jpeg', // Replace with your icon path
   }
   event.waitUntil(
     self.registration.showNotification('PWA push - peero dev api', options)
